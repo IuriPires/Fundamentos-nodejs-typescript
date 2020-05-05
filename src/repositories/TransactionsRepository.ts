cc27b232fa123ecbab607920e55f364ddbc4a108
Iuri Pires <iuripires.work@gmail.com>
2020-05-05 14:38:55 -0300

    desafio terminado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  value: number;
+  title: string;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance: Balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    this.transactions.forEach(transaction => {
+      if (transaction.type === 'income') {
+        balance.income += transaction.value;
+      } else if (transaction.type === 'outcome') {
+        balance.outcome += transaction.value;
+      }
+    });
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ value, title, type }: TransactionDTO): Transaction {
+    const dataObject = { value, title, type };
+
+    const transaction = new Transaction(dataObject);
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -8,8 +8,28 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ value, title, type }: Omit<Transaction, 'id'>): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+
+    let transaction;
+
+    if (type === 'income') {
+      transaction = this.transactionsRepository.create({
+        value,
+        title,
+        type,
+      });
+      return transaction;
+    }
+    if (type === 'outcome' && balance.total > value) {
+      transaction = this.transactionsRepository.create({
+        value,
+        title,
+        type,
+      });
+      return transaction;
+    }
+    throw Error('The outcome is greater than total');
   }
 }
 
